08dbb8976cc5fab9684106076957089d2b003bed
fastender <suerekli@live.de>
2025-06-19 22:52:53 +0300

    Update fast-search-card.js

--- dist/fast-search-card.js
+++ dist/fast-search-card.js
@@ -2292,11 +2292,10 @@ class FastSearchCard extends HTMLElement {
         const volume = state.attributes.volume_level ? Math.round(state.attributes.volume_level * 100) : 50;
         
         const pauseIcon = `<svg width="48px" height="48px" stroke-width="1" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="currentColor"><path d="M6 18.4V5.6C6 5.26863 6.26863 5 6.6 5H9.4C9.73137 5 10 5.26863 10 5.6V18.4C10 18.7314 9.73137 19 9.4 19H6.6C6.26863 19 6 18.7314 6 18.4Z" stroke="currentColor" stroke-width="1"></path><path d="M14 18.4V5.6C14 5.26863 14.2686 5 14.6 5H17.4C17.7314 5 18 5.26863 18 5.6V18.4C18 18.7314 17.7314 19 17.4 19H14.6C14.2686 19 14 18.7314 14 18.4Z" stroke="currentColor" stroke-width="1"></path></svg>`;
-        
         const playIcon = `<svg width="48px" height="48px" stroke-width="1" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="currentColor"><path d="M6.90588 4.53682C6.50592 4.2998 6 4.58808 6 5.05299V18.947C6 19.4119 6.50592 19.7002 6.90588 19.4632L18.629 12.5162C19.0211 12.2838 19.0211 11.7162 18.629 11.4838L6.90588 4.53682Z" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"></path></svg>`;
         
         const offClass = (!isPlaying && !isPaused) ? 'off' : '';
-        const activeClass = isPlaying ? 'active' : '';
+        const activeClass = ''; // DIESE ZEILE ÄNDERN
         const buttonTitle = isPlaying ? 'Pause' : 'Play';
         const displayValue = (!isPlaying && !isPaused) ? 'AUS' : volume + '%';
         const playPauseIcon = isPlaying ? pauseIcon : playIcon;
