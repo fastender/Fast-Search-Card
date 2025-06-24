f669e1b795a43c9e2086d97d90b8881238a0117b
fastender <suerekli@live.de>
2025-06-24 07:12:23 +0200

    Update fast-search-card.js

--- dist/fast-search-card.js
+++ dist/fast-search-card.js
@@ -2918,15 +2918,15 @@ class FastSearchCard extends HTMLElement {
                         <svg width="48px" height="48px" stroke-width="1" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="currentColor"><path d="M7 12L17 12" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"></path><path d="M7 8L13 8" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"></path><path d="M3 20.2895V5C3 3.89543 3.89543 3 5 3H19C20.1046 3 21 5V15C21 16.1046 20.1046 17 19 17H7.96125C7.35368 17 6.77906 17.2762 6.39951 17.7506L4.06852 20.6643C3.71421 21.1072 3 20.8567 3 20.2895Z" stroke="currentColor" stroke-width="1"></path></svg>
                     </button>
                 </div>
-
-                </div>
                 <div class="device-control-presets music-assistant-presets" data-is-open="false">
                     ${this.getMusicAssistantHTML(item)}
                 </div>
+
                 <div class="device-control-presets tts-presets" data-is-open="false">
-                    <div class="preset-content">TTS coming soon</div>
+                    TTS coming soon
                 </div>
-            </div>
+                </div>
+                
         `;
     }        
     
