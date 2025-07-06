5a4a77a22bd49f101088adc941749c8029d258b7
fastender <suerekli@live.de>
2025-07-06 15:55:29 +0200

    Update fast-search-card.js

--- dist/fast-search-card.js
+++ dist/fast-search-card.js
@@ -261,6 +261,32 @@ class FastSearchCard extends HTMLElement {
       <path d="M3 5m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v4a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z" />
       <path d="M7 13v-3a1 1 0 0 1 1 -1h8a1 1 0 0 1 1 1v3" />
     </svg>`;
+
+    // Script, Automation, Scene Icons
+    static SCRIPT_SVG = `<svg xmlns="http://www.w3.org/2000/svg" width="39" height="39" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round">
+      <path d="M4 19V5C4 3.89543 4.89543 3 6 3H19.4C19.7314 3 20 3.26863 20 3.6V16.7143"/>
+      <path d="M6 17L20 17"/><path d="M6 21L20 21"/>
+      <path d="M6 21C4.89543 21 4 20.1046 4 19C4 17.8954 4.89543 17 6 17"/>
+      <path d="M9 7L15 7"/>
+    </svg>`;
+    
+    static AUTOMATION_SVG = `<svg xmlns="http://www.w3.org/2000/svg" width="39" height="39" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round">
+      <path d="M15 12L15 15"/><path d="M12 3V6"/><path d="M18 12L18 15"/>
+      <path d="M12 18L21 18"/><path d="M18 21H21"/><path d="M6 12H9"/>
+      <path d="M6 6.01111L6.01 6"/><path d="M12 12.0111L12.01 12"/>
+      <path d="M3 12.0111L3.01 12"/><path d="M12 9.01111L12.01 9"/>
+      <path d="M12 15.0111L12.01 15"/><path d="M15 21.0111L15.01 21"/>
+      <path d="M12 21.0111L12.01 21"/><path d="M21 12.0111L21.01 12"/>
+      <path d="M21 15.0111L21.01 15"/>
+    </svg>`;
+    
+    static SCENE_SVG = `<svg xmlns="http://www.w3.org/2000/svg" width="39" height="39" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round">
+      <path d="M20.5096 9.54C20.4243 9.77932 20.2918 9.99909 20.12 10.1863C19.9483 10.3735 19.7407 10.5244 19.5096 10.63C18.2796 11.1806 17.2346 12.0745 16.5002 13.2045C15.7659 14.3345 15.3733 15.6524 15.3696 17C15.3711 17.4701 15.418 17.9389 15.5096 18.4C15.5707 18.6818 15.5747 18.973 15.5215 19.2564C15.4682 19.5397 15.3588 19.8096 15.1996 20.05C15.0649 20.2604 14.8877 20.4403 14.6793 20.5781C14.4709 20.7158 14.2359 20.8085 13.9896 20.85C13.4554 20.9504 12.9131 21.0006 12.3696 21C11.1638 21.0006 9.97011 20.7588 8.85952 20.2891C7.74893 19.8194 6.74405 19.1314 5.90455 18.2657C5.06506 17.4001 4.40807 16.3747 3.97261 15.2502C3.53714 14.1257 3.33208 12.9252 3.36959 11.72C3.4472 9.47279 4.3586 7.33495 5.92622 5.72296C7.49385 4.11097 9.60542 3.14028 11.8496 3H12.3596C14.0353 3.00042 15.6777 3.46869 17.1017 4.35207C18.5257 5.23544 19.6748 6.49885 20.4196 8C20.6488 8.47498 20.6812 9.02129 20.5096 9.52V9.54Z"/>
+      <path d="M8 16.01L8.01 15.9989"/><path d="M6 12.01L6.01 11.9989"/>
+      <path d="M8 8.01L8.01 7.99889"/><path d="M12 6.01L12.01 5.99889"/>
+      <path d="M16 8.01L16.01 7.99889"/>
+    </svg>`;
+    
     
     constructor() {
         super();
@@ -6811,6 +6837,18 @@ class FastSearchCard extends HTMLElement {
                 FastSearchCard.CLIMATE_ON_SVG : 
                 FastSearchCard.CLIMATE_OFF_SVG;
         }
+
+        if (item.domain === 'script') {
+            return FastSearchCard.SCRIPT_SVG;
+        }
+        
+        if (item.domain === 'automation') {
+            return FastSearchCard.AUTOMATION_SVG;
+        }
+        
+        if (item.domain === 'scene') {
+            return FastSearchCard.SCENE_SVG;
+        }        
         
         // Fallback für andere Domains
         return item.icon;
