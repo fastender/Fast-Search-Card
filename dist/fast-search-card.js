cc023a24290aa86f4b2317f0382604ce69ac73dc
fastender <suerekli@live.de>
2025-07-07 16:49:12 +0200

    Update fast-search-card.js

--- dist/fast-search-card.js
+++ dist/fast-search-card.js
@@ -9558,8 +9558,6 @@ class FastSearchCard extends HTMLElement {
                         </div>
                     </div>
                     
-                    // In der displayActiveTimers() Funktion, ersetzen Sie:
-                    
                     <button class="timer-delete" data-timer-id="${timer.schedule_id}" title="Timer löschen">
                         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
