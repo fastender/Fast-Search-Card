dc9b059f47787d9aa5ecadbb3852465a24ee4714
fastender <suerekli@live.de>
2025-07-07 16:51:23 +0200

    Update fast-search-card.js

--- dist/fast-search-card.js
+++ dist/fast-search-card.js
@@ -2989,8 +2989,9 @@ class FastSearchCard extends HTMLElement {
             }
             
             .timer-delete {
-                width: 32px;
-                height: 32px;
+                width: 40px;
+                height: 40px;
+                color: var(--text-primary);
                 border: none;
                 background: rgba(255, 255, 255, 0.1);
                 border-radius: 50%;
